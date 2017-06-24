6e04d23e1075cdcb87af90a622c8db773f261d72
jiaowagn <wangjiaohere@gmail.com>
2017-06-24 19:02:21 +0800

    cannot add same item in cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,12 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功将 #{@product.title} 加入购物车"
+    if current_cart.products.include?(@product)
+      flash[:warning] = "你的购物车中已此商品"
+    else
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功将 #{@product.title} 加入购物车"
+    end
     redirect_to :back
   end
 
