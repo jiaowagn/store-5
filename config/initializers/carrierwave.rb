4670c0fec7c8e21ffea3fea43cdcf9f587febcb0
jiaowagn <wangjiaohere@gmail.com>
2017-06-25 14:14:20 +0800

    change protocol

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
   config.qiniu_bucket       = ENV["qiniu_bucket"]
   config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size   = 4*1024*1024
-  config.qiniu_protocol     = "http"
+  config.qiniu_protocol    = "http"
   config.qiniu_up_host      = "http://up-z1.qiniu.com"
 end
